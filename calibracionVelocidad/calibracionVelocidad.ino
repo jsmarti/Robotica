47567b7754fc45ea878d623709524203cfbfbe3d
jsmarti <jsmarti@hotmail.com>
2015-04-14 14:55:41 -0500

    Adicion de calibracion de corriente

---
+++ calibracionVelocidad/calibracionVelocidad.ino
@@ -0,0 +1,15 @@
+int sensorCorriente = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+
+   double voltajeMedido = analogRead(sensorCorriente);
+   double conversion = voltajeMedido*5/1023;
+   
+   Serial.print("Voltaje Medido: "+String(conversion));
+
+}
